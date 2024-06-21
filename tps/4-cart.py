5911766ace0981b5bf8c5fb5f602b5e5914d5cf1
salif437 <tsalif190@gmail.com>
2024-06-21 22:14:38 +0000

    travail salif
    j'ai utilisé isdigit

--- tps/4-cart.py
+++ tps/4-cart.py
@@ -12,25 +12,25 @@ while choix != "5":
     print("5. Quitter")
     # Saisie du choix par l'utilisateur
     choix = input("Quel est votre choix? ")
-    # Si le choix est 1
-    if choix == "1":
-       print("l'article a été bien ajouté dans votre panier")
-    # Si le choix est 2
-    elif choix == "2":
-        print("l'article a été bien supprimé de votre panier")
-    # Si le choix est 3
-    elif choix == "3":
-        print("voici la liste des articles du panier")
+    if choix.isdigit() :
+        # Si le choix est 1
+        if choix == "1":
+            print("l'article a été bien ajouté dans votre panier")
+        # Si le choix est 2
+        elif choix == "2":
+            print("l'article a été bien supprimé de votre panier")
+        # Si le choix est 3
+        elif choix == "3":
+            print("voici la liste des articles du panier")
         
-    # Si le choix est 4
-    elif choix == "4":
-        print("votre panier a bien été supprimé")
-    # Si le choix est 5
-    elif choix == "5":
-        # Affichage d'un message de fin
-        print("Merci pour votre visite!!!")
-      
-    # Sinon
+        # Si le choix est 4
+        elif choix == "4":
+            print("votre panier a bien été supprimé")
+        # Si le choix est 5
+        elif choix == "5":
+            # Affichage d'un message de fin
+            print("Merci pour votre visite!!!")
+      # Sinon
     else:
         # Affichage d'un message d'erreur
         print("mauvaise entrée, veuillez entrer une valeur entre 1 et 5")
