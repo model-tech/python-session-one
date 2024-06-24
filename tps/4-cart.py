e351fc58e35812d282878a1d61a911028ab58274
Yacinn12 <josephdiasso6@gmail.com>
2024-06-24 20:58:23 +0000

    travail yacinn

---
+++ tps/4-cart.py
@@ -0,0 +1,23 @@
+### Ecrire un menu qui va afficher un panier numérique à l'utilisateur
+
+choix =""
+while choix !="5":
+    print("1- Ajouter un article dans le panier") 
+    print("2- Supprimer un article du panier ") 
+    print("3- Afficher tous les articles") 
+    print("4- Vider le panier") 
+    print("5- Quitter") 
+    choix = input("Quel est votre choix ?") 
+    if choix.isdigit() and int(choix) in range(1,6):
+        if choix == "1":
+            print("L'article a bien été ajouté dans votre panier") 
+        elif choix =="2":
+            print("L'article a bien été supprimé de votre panier")
+        elif choix == "3":
+            print("Voici la liste des articles du panier") 
+        elif choix == "4":
+            print("Votre panier a été bien supprimé") 
+        elif choix == "5":
+            print("Merci pour votre visite") 
+    else:
+        print("Mauvaise entrée veuillez entrer une valeur entre 1 et 5") 
