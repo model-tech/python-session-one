7bb585692cbfa3bc1d6651c04643405775fd0339
MamadouAmar <mamadou.amar1980@gmail.com>
2024-06-24 13:34:42 +0000

    exrcice 1 termine

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,1 +1,16 @@
-print("Bonjour la classe")
+# Je demande à l'utilisateur d'entrer un chiffre
+chiffre = input("entrer un chiffre entre 2 et 9 ")
+# Je vérifie que la valeur entree est un chiffre
+if (chiffre.isdigit()):
+    chiffre = int(chiffre)
+    if (chiffre >= 2) and (chiffre <= 9):
+        for i in range(1, 13):
+            print(f"{chiffre} x {i} = {chiffre * i}")
+    else :
+        print("entrer un chiffre entre 2 et 9")
+else :
+    print("veuillez entrer un chiffre")        
+# si c'est un chiffre je verifie que c'est ente 2 et 9
+
+# Sinon j'affiche le message "entre un chiffre entre 2 et 9"
+
