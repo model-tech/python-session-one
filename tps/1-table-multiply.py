90c8ced103c35f98a01f433a47a103a22bb534dc
Yacinn12 <josephdiasso6@gmail.com>
2024-06-24 18:54:41 +0000

    travail yacinn

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,2 +1,15 @@
-print("Bonjour ceci est un test")
+### Entrer un nombre compris entre 2 et 9 
+# si le nombre entré remplie les conditions, 
+# effectuer une table de multiplication de ce nombre ###
+
+nombre_choisi = input(" Entrer un nombre comprit entre 2 et 9 : ")
+if nombre_choisi.isdigit() :
+    nombre = int(nombre_choisi)
+    if 2 <= nombre <= 9 :
+        for i in range(1,13) :
+            print(f"{nombre} * {i} = {nombre * i}") 
+    else :
+        print("Entrer un nombre entre 2 et 9") 
+else :
+    print("Entrer un nombre numérique")
 
--- tps/2-invoice.py
+++ tps/2-invoice.py
@@ -1,1 +1,29 @@
-print("second essaie")
+### La résolution de cet exercice nous amenera à produire un programme
+# Le programme nous demandera le montant de l'achat et la quantité acheté 
+# Après avoir entré les informations necessaire le programme exécutura le 
+# calcul et nous verrons apparaitre le montant ht la quantité la tva 
+# et le montant ttc .###
+
+ 
+prix_achat = input("Veuillez entrer le prix d'achat:") 
+quantite_acheté = input("Veuillez entrer la quantité acheté :") 
+tva = 0.18
+print("*****Bienvenue sur JUMIO S.A.R.L*****")
+
+if prix_achat.isdigit() and quantite_acheté.isdigit():
+    p = int(prix_achat) 
+    q = int(quantite_acheté) 
+    print("Votre facture :")
+    print(f"P.U : {p}CFA") 
+    print(f"Quantité : {q}") 
+    montant_ht = p * q
+    print(f"Montant HT : {montant_ht} CFA") 
+    montant_tva = montant_ht * tva 
+    print(f"TVA : {montant_tva} CFA") 
+    montant_ttc = montant_ht + montant_tva 
+    print(f"Montant TTC : {montant_ht + montant_tva}CFA")
+    print("JUMIO vous remercie pour votre achat et vous souhaites de Joyeuses fête !!!!") 
+else : 
+     print(" Veuillez entrer une valeur numérique ")
+
+   
