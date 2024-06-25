f88d39b6392ff08819b0f7b152cfd7d3abbc7e3a
audeflo <audendubuisi69@gmail.com>
2024-06-25 16:43:56 +0000

    modification terminée

--- tps/4-cart.py
+++ tps/4-cart.py
@@ -12,17 +12,7 @@ while True:
 
     while not (choice.isdigit() and int(choice) in range(1,6)):
         print("Erreur! Veuillez choisir parmis les optionsd disponibles")
-        print("""
-            Choisissez parmi les 5 options suivantes: 
-
-            1- Ajouter un article dans le panier
-            2- Supprimer un article du panier
-            3- Afficher tous les articles 
-            4- Vider le panier
-            5- Quitter""")
-
-        choice= input("Quel est votre choix?:   ")
-
+        
     choice=int(choice)
     if choice==1:
         print("l'article a été bien ajouté dans votre panier")
