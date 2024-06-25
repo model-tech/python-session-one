594ea5220e03e3e95bc963d19a23e821716f44a4
audeflo <audendubuisi69@gmail.com>
2024-06-25 16:41:04 +0000

    tp 4

---
+++ tps/4-cart.py
@@ -0,0 +1,40 @@
+while True:
+    print("""
+    Choisissez parmi les 5 options suivantes: 
+
+    1- Ajouter un article dans le panier
+    2- Supprimer un article du panier
+    3- Afficher tous les articles 
+    4- Vider le panier
+    5- Quitter""")
+
+    choice= input("Quel est votre choix?:   ")
+
+    while not (choice.isdigit() and int(choice) in range(1,6)):
+        print("Erreur! Veuillez choisir parmis les optionsd disponibles")
+        print("""
+            Choisissez parmi les 5 options suivantes: 
+
+            1- Ajouter un article dans le panier
+            2- Supprimer un article du panier
+            3- Afficher tous les articles 
+            4- Vider le panier
+            5- Quitter""")
+
+        choice= input("Quel est votre choix?:   ")
+
+    choice=int(choice)
+    if choice==1:
+        print("l'article a été bien ajouté dans votre panier")
+    elif choice==2:
+        print("l'article a été bien supprimé de votre panier")
+        choice= input("Quel est votre choix?:   ")
+    elif choice==3:
+        print("voici la liste des articles du panier")
+        choice= input("Quel est votre choix?:   ")
+    elif choice==4:
+        print("voici la liste des articles du panier")
+        choice= input("Quel est votre choix?:   ")
+    else: 
+        print("Merci pour votre visite!!!")
+        break
