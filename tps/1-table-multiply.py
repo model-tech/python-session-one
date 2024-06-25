4334d5e1f48fa839860011584b65e933c57d6e7b
EmmanuelBouadi <kopoinbouadi@cesag.edu.sn>
2024-06-25 12:47:58 +0000

    Modification exercice 1

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,10 +1,10 @@
 while True:
-        nombre = int(input("Entrez un chiffre entre 2 et 9 : "))
-        if nombre in range(2,10):
-            break
-        else:
-            print("Veuillez entrer un chiffre entre 2 et 9.")
-print("Voici la table de multiplication de,", nombre,":")
+    chiffre = input("Entrez un chiffre entre 2 et 9 : ")
+    if chiffre.isdigit() and 2 <= int(chiffre) <= 9:
+        break
+    else:
+        print("Vous devez entrer un chiffre entre 2 et 9.")
+C = int(chiffre)
+print("Table de multiplication de", C, ":")
 for i in range(1, 11):
-    produit = nombre * i
-    print(f"{nombre} x {i} = {produit}")
+    print(f"{C} x {i} = {C * i}")
