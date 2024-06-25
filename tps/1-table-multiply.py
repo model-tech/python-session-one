10c412095933b88454d98a424088d425b40853a4
traarsene <traarsene123@gmail.com>
2024-06-25 14:41:42 +0000

    Travail sur les tables de multication

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,12 +1,11 @@
-def afficher_table_multiplication():
-    try:
-        chiffre = int(input("Entrez un chiffre entre 2 et 9 : "))
-        if 2 <= chiffre <= 9:
-            print(f"Table de multiplication pour {chiffre} :")
-            for i in range(1, 13):
-                resultat = chiffre * i
-                print(f"{chiffre} x {i} = {resultat}")
-        else:
-            print("Le chiffre doit être entre 2 et 9.")
-    except ValueError:
+try:
+    chiffre = int(input("Entrez un chiffre entre 2 et 9 : "))
+    if 2 <= chiffre <= 9:
+        print(f"Table de multiplication pour {chiffre} :")
+        for i in range(1, 13):
+            resultat = chiffre * i
+            print(f"{chiffre} x {i} = {resultat}")
+    else:
+        print("Le chiffre doit être entre 2 et 9.")
+except ValueError:
         print("Veuillez entrer un nombre valide.")
