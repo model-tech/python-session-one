7dfb42734fadc5556838d1ea2c8c5601bd9ca6f8
GAYERI681 <gayerigildas681@gmail.com>
2024-06-27 22:01:43 +0000

    travail de gildas

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -5,9 +5,9 @@
 nombre_choisi = input("Entrer un nombre compris entre 2 et 9 : ")
 if nombre_choisi.isdigit():
     nombre = int(nombre_choisi)
-    if 2 <= nombre <= 9 :
+    if (nombre >=2) and (nombre<=9):
         for i in range(1,13):
-            print(f"{nombre} * {i} = {nombre*i}")
+            print(f"{nombre} x {i} = {nombre*i}")
     else:
             print("Entrer un nombre entre 2 et 9")
 else:
