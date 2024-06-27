bdb3b02d88772276eea2b4bc01e4b1a8ec20b9cc
Tini-Cedric-Bonzi <bonzicedric6@gmail.com>
2024-06-27 21:31:04 +0000

    travail de tini

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,2 +1,15 @@
-print("Bonjour je suis présent")
+### veillez entrez un nombre compris entre 2 et 9
+# si il entre un nombre compris entre 2 et 9
+#effectuez la table de multiplication de ce nombre ###
 
+nombre_choisi = input ("entrez un nombre compris entre 2 et 9: ")
+if nombre_choisi.isdigit(): 
+    nombre= int(nombre_choisi)
+    if (nombre >=2) and (nombre <=9):
+        for i in range (1,13):
+            print(f"{nombre} x {i}) = {nombre*i}")
+    else:
+            print("enter un nombre entre 2 et 9")
+   else
+          print("entrez un nombre numérique") 
+ 
