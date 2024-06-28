59481a96d36b29faee807a04f3ce3ef01a2e299e
Fatima-sur-git <0753habiba.fatima@gmail.com>
2024-06-28 22:08:46 +0000

    table de multiplication

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,28 +1,8 @@
 chiffre = input("Veuillez svp entrer un chiffre entre 2 et 9 : ")
 
-
-while not chiffre in range(2,10) :
-    chiffre = input("Veuillez svp entrer un chiffre entre 2 et 9 : ")
+while not (chiffre.isdigit() and 2 <= int(chiffre) <= 9) :
+    chiffre = input("Entrée invalide! Veuillez svp entrer un chiffre entre 2 et 9 : ")
+else:
     for i in range(1,13):
-        multiply = chiffre * i
-        print(f"{chiffre} * {i} = {multiply}")
-#elif  not chiffre.isalpha :
-    #chiffre = input("Veuillez svp entrer un chiffre entre 2 et 9 : ")
-    # else:
-    # for i in range(1,13):
-    # multiply = chiffre * i
-    #  print(f"{chiffre} * {i} = {multiply}")
-
-
-
-    
-
-    #break
-#else:
-    #chiffre = input("Veuillez svp entrer un chiffre entre 2 et 9 : ")
-    
-    #for chiffre in range(2,10):
-        #for i in range(1,13):
-            #multiply = chiffre * i
-            #print(f"{chiffre} *{i} = {multiply}")
-        #break
+        mutiply = chiffre*i
+        print(f"{chiffre} * {i}  = {mutiply}")
