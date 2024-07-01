396c0b70c2bcbab9a8910d9836e099fab6a5b887
fatima2605 <fatimagounougo7@gmail.com>
2024-07-01 14:56:29 +0000

    travail fatima

--- tps/2-invoice.py
+++ tps/2-invoice.py
@@ -1,13 +1,13 @@
 a=print("Bienvenue sur JUMIO S.A.R.L")
 prix_unitaire=float(input("entrez le  prix unitaire"))
 quantite=float(input("entrez la quantite"))
-prix_ht=float(input("entrez le montant ht"))
-prix_tva=float(input("entrez le montant tva")) 
-prix_ttc=float(input("entrez le montant ttc"))
+prix_ht=prix_unitaire*quantite
+prix_tva=prix_ht*0.18
+prix_ttc=prix_ht+prix_tva
 print("\nVOTRE FACTURE")
-print(f"p.u :{prix_unitaire:3000f} CFA")
-print(f"quantite :{quantite:3}")
-print("montant HT est: , 9000F CFA")
-print("TVA est: , 1620f CFA")
-print("montant TTC est: , 10620f CFA") 
+print(f"p.u :{prix_unitaire:} CFA")
+print(f"quantite :{quantite:}")
+print(f"montant HT est:{prix_ht}  CFA")
+print(f"TVA est:{prix_tva} CFA")
+print(f"montant TTC est:{prix_ttc} CFA") 
 print("JUMIO vous remercie pour votre achat et vous souhaite de joyeuses fetes!")
