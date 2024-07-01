3bf60d0612c7a8b8d31e51a3e6451fa29d376522
fatima2605 <fatimagounougo7@gmail.com>
2024-07-01 14:13:49 +0000

    invoice.py

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,3 +1,7 @@
-a= input("entrez un nombre ")
-
-
+a=input("entrez un chiffre entre 2 et 9 ")
+while not (a.isdigit() and 2<=int(a)<=9) :
+    print("erreur")
+    a=input("entrez un chiffre entre 2 et 9 ")
+b=int(a)
+for i in range(1,13) :
+    print(f"{b}*{i}={b*i}")
