77dfac86295da56910a59a433b5304005cf6dedb
hawaou-sid <hawaousid99@gmail.com>
2024-07-02 14:03:54 +0000

    TP Hawaou

--- tps/1-table-multiply.py
+++ tps/1-table-multiply.py
@@ -1,2 +1,7 @@
-print("Bonjour je suis présent")
-print("hello") 
+h=input("veuillez entrer un nombre 2 et 9:")
+while not (h.isdigit and 2<=int (h)<=9) :
+  h=input("veuillez entrer un nombre 2 et 9:")
+    
+a=int(h)
+for i in range (1,13):
+    print(f"{a}*{i}={a*i}")
