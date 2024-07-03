3536e602de36e50f9a932c5146e3eff4cd468c4d
M-Mafia-lamine <chaiboumoussalamine337@gmail.com>
2024-07-03 19:15:14 +0000

    valider book.py

---
+++ book.py
@@ -0,0 +1,11 @@
+#
+#
+print("****Bienvenue sur JUMIO S.A.R.L****")
+print("""Votre facture: 
+P.U :  2000 CFA 
+Quantité : 5 
+Montant HT :  10000 CFA 
+TVA :  1800 CFA
+Montant TTC : 11800 CFA
+JUMIO vous Merci pour votre achat et vous souhaites de Joyeuses fêtes 
+!!!!!!! """)
